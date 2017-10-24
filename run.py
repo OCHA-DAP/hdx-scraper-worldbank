e63d763c9ec85d9020c4c85ce15ef95e1798a9e4
Michael Rans <rans@email.com>
2017-10-24 11:44:33 +0200

    use with

--- run.py
+++ run.py
@@ -21,23 +21,23 @@ def main():
     """Generate dataset and create it in HDX"""
 
     base_url = Configuration.read()['base_url']
-    downloader = Download(status_forcelist=[400, 429, 500, 502, 503, 504])
-    indicators, tags = get_indicators_and_tags(base_url, downloader, Configuration.read()['indicator_list'])
-    folder = gettempdir()
-    topline_indicator_names = Configuration.read()['topline_indicators']
+    with Download(status_forcelist=[400, 429, 500, 502, 503, 504]) as downloader:
+        indicators, tags = get_indicators_and_tags(base_url, downloader, Configuration.read()['indicator_list'])
+        folder = gettempdir()
+        topline_indicator_names = Configuration.read()['topline_indicators']
 
-    country_isos = list()
-    topline_indicators = list()
-    for countryiso, countryname in get_countries(base_url, downloader):
-        dataset, country_topline_indicators = generate_dataset(base_url, downloader, countryiso, countryname,
-                                                               indicators, topline_indicator_names)
-        if dataset is not None:
-            logger.info('Adding %s' % countryname)
-            dataset.add_tags(tags)
-            dataset.update_from_yaml()
-            dataset.create_in_hdx()
-            topline_indicators.extend(country_topline_indicators)
-            country_isos.append(countryiso)
+        country_isos = list()
+        topline_indicators = list()
+        for countryiso, countryname in get_countries(base_url, downloader):
+            dataset, country_topline_indicators = generate_dataset(base_url, downloader, countryiso, countryname,
+                                                                   indicators, topline_indicator_names)
+            if dataset is not None:
+                logger.info('Adding %s' % countryname)
+                dataset.add_tags(tags)
+                dataset.update_from_yaml()
+                dataset.create_in_hdx()
+                topline_indicators.extend(country_topline_indicators)
+                country_isos.append(countryiso)
 
     dataset = generate_topline_dataset(folder, topline_indicators, country_isos)
     logger.info('Adding topline indicators')
