49f5d7d37cff1b2e263c16ddb49761b4233e8e29
Michael Rans <rans@email.com>
2017-11-08 09:53:10 +0100

    Add email output

--- run.py
+++ run.py
@@ -8,13 +8,14 @@ import logging
 from os.path import join
 from tempfile import gettempdir
 
-from hdx.facades import logging_kwargs
 from hdx.hdx_configuration import Configuration
 from hdx.utilities.downloader import Download
 
 from worldbank import generate_dataset_and_showcase, get_countries, get_indicators_and_tags, generate_topline_dataset
 
+from hdx.facades import logging_kwargs
 logging_kwargs['smtp_config_yaml'] = join('config', 'smtp_configuration.yml')
+
 from hdx.facades.hdx_scraperwiki import facade
 
 logger = logging.getLogger(__name__)
